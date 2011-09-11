5efe3ece31a4e52805a4cb9ce8ef205bb7f5c74c
Rabih Nassar <rabih.nassar@apstrata.com>
2011-09-11 00:54:51 -0400

    added saveChatRoom and listChatRooms

--- persistence.js
+++ persistence.js
@@ -1,13 +1,21 @@
 var apstrata = require('./lib/apstrata')
 
+function escapeKey(value) {
+	
+}
+
+function unescapeKey(value) {
+
+}
+
+
 exports.saveChatRoom = function(name, success, failure) {
 	attrs = {
 		credentials: credentials.apstrata,
-		operation: "SaveDocument", 
+		operation: "RunScript", 
 		params: {
-			"apsdb.store": "DefaultStore",
-			"docType": "chatroom",
-			"apsdb.documentKey": name
+			"apsdb.scriptName": "saveChatRoom",
+			"name": name
 		}, 
 		success: function(json) {
 			debug.log(json)
@@ -16,8 +24,7 @@ exports.saveChatRoom = function(name, success, failure) {
 			debug.log(json)
 		}
 	}
-	// call apstrata
-	var a = 1
+
 	apstrata.call(attrs)
 }
 
@@ -43,7 +50,26 @@ exports.saveUser = function(_id, success, failure) {
 
 exports.listChatRooms = function(success, failure) {
 	var chatRooms = {}
-	return chatRooms
+	
+	attrs = {
+		credentials: credentials.apstrata,
+		operation: "Query", 
+		params: {
+			"apsdb.store": "DefaultStore",
+			"apsdb.query": 'docType=\"chatroom\"',
+			"apsdb.queryFields": "apsdb.documentKey, name",
+			"apsdb.pageNumber": "1",
+			"apsdb.resultsPerPage": "50"
+		}, 
+		success: function(json) {
+			debug.log(json.response.result.documents)
+		}, 
+		failure: function(json) {
+			debug.log(json)
+		}
+	}
+
+	apstrata.call(attrs)	
 }
 
 exports.getUser = function(_id, success, failure) {
