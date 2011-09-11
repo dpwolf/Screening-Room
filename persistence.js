02a28bca6df9c86e196b00f1928ff664e667aa24
Rabih Nassar <rabih.nassar@apstrata.com>
2011-09-10 23:33:16 -0400

    updated persistence.js

---
+++ persistence.js
@@ -0,0 +1,50 @@
+var apstrata = require('./lib/apstrata')
+
+exports.saveChatRoom = function(name, success, failure) {
+	attrs = {
+		credentials: credentials.apstrata,
+		operation: "SaveDocument", 
+		params: {
+			"apsdb.store": "DefaultStore",
+			"docType": "chatroom",
+			"apsdb.documentKey": name
+		}, 
+		success: function(json) {
+			debug.log(json)
+		}, 
+		failure: function(json) {
+			debug.log(json)
+		}
+	}
+
+	apstrata.call(attrs)
+}
+
+exports.saveUser = function(_id, success, failure) {
+	attrs = {
+		credentials: credentials.apstrata,
+		operation: "SaveDocument", 
+		params: {
+			"apsdb.store": "DefaultStore",
+			"docType": "user",
+			"apsdb.documentKey": _id
+		}, 
+		success: function(json) {
+			debug.log(json)
+		}, 
+		failure: function(json) {
+			debug.log(json)
+		}
+	}
+
+	apstrata.call(attrs)
+}
+
+exports.listChatRooms = function(success, failure) {
+	var chatRooms = {}
+	return chatRooms
+}
+
+exports.getUser = function(_id, success, failure) {
+
+}
